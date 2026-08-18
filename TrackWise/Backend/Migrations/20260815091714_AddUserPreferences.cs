6f5bc543fd4d212b01e8b1bd8ce29f03598059b6
Rathish <rathish11901@gmail.com>
2026-08-18 23:20:55 +0530

    .

---
+++ TrackWise/Backend/Migrations/20260815091714_AddUserPreferences.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "$");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "en");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Theme",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "default");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Theme",
+                table: "Users");
+        }
+    }
+}
